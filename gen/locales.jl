8930859ff6767f0004a4f2f164b6d910de61110e
WooKyoung Noh <wookay.noh@gmail.com>
2022-05-26 13:56:14 +0900

    Data Dragon 12.10.1

--- gen/locales.jl
+++ gen/locales.jl
@@ -1,8 +1,8 @@
 # generated
 type = "champion"
 format = "standAloneComplex"
-version = v"11.17.1"
+version = v"12.10.1"
 for locale in ("en_US", "ko_KR")
-    gendir = normpath(@__DIR__, "11.17.1", "generated", locale)
+    gendir = normpath(@__DIR__, "12.10.1", "generated", locale)
     include(normpath(gendir, "module.jl"))
 end
